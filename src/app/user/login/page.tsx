16baa5d59bad15e5c35a28f8cd70976a7880a6ed
hyobin0726 <hyobin0726@naver.com>
2024-03-14 03:09:17 +0900

    style : 로그인화면

--- src/app/user/login/page.tsx
+++ src/app/user/login/page.tsx
@@ -1,39 +1,42 @@
 import HeaderTitle from '@/components/ui/HeaderTitle'
+import React from "react";
 
 function Login() {
     return (
         <main>
             <HeaderTitle title="로그인" />
             <div>
-                <form>
-                    <div>
-                        <label>
-                            <input type="text" placeholder="아이디" />
-                        </label>
+                <form className='  p-8 flex flex-col  '>
+                    <div className=' mb-1'>
+                        <input type="text" placeholder="아이디" className=' mt-1 p-2 w-full h-12  border text-sm '/>
+                        <input type="text" placeholder="비밀번호" className='p-2 w-full h-12  border text-sm'/>
 
-                        <label>
-                            <input type="text" placeholder="비밀번호" />
-                        </label>
                     </div>
 
                     <div>
-                        <span>
-                            <input type="checkbox" value="Y"></input>
-                            <label>아이디 저장</label>
+                        <span className='ml-3'>
+                            <input type="checkbox" value="" className=' rounded-full checked:bg-red-500'></input>
+                            <label  className="ml-1  text-sm">아이디 저장</label>
                         </span>
                     </div>
                     <div>
-                        <button type="submit">로그인</button>
+                        <button
+                            type="submit" className='mt-5 w-full  h-12  text-white  bg-red-500 font-bold' >
+                            로그인
+                        </button>
                     </div>
 
-                    <div>
+                    <div className=' flex justify-center mt-4 space-x-1 text-xs'>
                         <a>아이디 찾기</a>
+                        <span >|</span>
                         <a>비밀번호 찾기</a>
+                        <span >|</span>
                         <a>회원가입</a>
                     </div>
                 </form>
+          
 
-                <ul>
+                <ul className=' flex justify-center mt-4 space-x-5 text-xs'>
                     <li>
                         <a>
                             <span>
@@ -84,16 +87,17 @@ function Login() {
                     </li>
                 </ul>
 
-                <div>
-                    <span>AD</span>
-                    <button type="submit">
+                <div className=' p-4 mt-20 '>
+                    
+                    <button type="submit" className='relative w-full  h-12  bg-black text-white text-sm font-bold'>
                         휴대폰 간편 로그인
-                        <span>광고</span>
+                        <span className=' absolute top-0 right-2 text-white text-xs'>AD</span>
+                        <span className='text-gray-400 text-xs ml-1 '>광고</span>
                     </button>
                 </div>
 
-                <a>
-                    <span>비회원 조회하기</span>
+                <a className=' flex mt-10 '>
+                    <span className='flex justify-center w-full  h-10  text-gray-400 text-xs' >비회원 조회하기</span>
                 </a>
             </div>
         </main>
