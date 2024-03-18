f3ecd537fee603eddd5d6610100925e30d71e119
myonjin <kjinabc@gmail.com>
2024-03-19 01:16:00 +0900

    init : cors 허용

--- src/main/java/com/tyranno/ssg/SsgApplication.java
+++ src/main/java/com/tyranno/ssg/SsgApplication.java
@@ -2,14 +2,25 @@ package com.tyranno.ssg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableWebMvc
 @SpringBootApplication
 public class SsgApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SsgApplication.class, args);
 	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				//local , server cors 허용
+				registry.addMapping("/**").allowedOrigins("https://tyranno.site", "http://localhost:3000");
 
+			}
+		};
+	}
 }
