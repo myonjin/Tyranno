b068e446739af4f9603879f6f391a99e57d024ce
kjinabc <kjinabc@gmail.com>
2024-03-19 17:50:28 +0900

    Fix : Cors 수정

--- src/main/java/com/tyranno/ssg/SsgApplication.java
+++ src/main/java/com/tyranno/ssg/SsgApplication.java
@@ -2,9 +2,11 @@ package com.tyranno.ssg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
+@EnableJpaAuditing
 @SpringBootApplication
 public class SsgApplication {
 
