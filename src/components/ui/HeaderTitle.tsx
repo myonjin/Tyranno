3a020b310c89f4ca326708f54f27eacaf5cf2ae7
jmlee119 <jmlee119@naver.com>
2024-03-19 11:08:51 +0900

    feat : header button 추가

--- src/components/ui/HeaderTitle.tsx
+++ src/components/ui/HeaderTitle.tsx
@@ -1,9 +1,14 @@
+'use client'
 import BackIcon from '@/images/back'
-
+import { useRouter } from 'next/navigation'
 export default function HeaderTitle({ title }: { title: string }) {
+    const router = useRouter()
+    const back = () => {
+        router.back()
+    }
     return (
         <div className="flex items-center h-10  border-b  border-gray-400">
-            <span className="ml-4 ">
+            <span className="ml-4" onClick={back}>
                 <BackIcon />
             </span>
             <p className="flex-grow font-bold text-lg text-center">{title}</p>
