1cc9b8f033460066a106fe1768da1da9dec5a26b
hyobin0726 <hyobin0726@naver.com>
2024-03-20 09:30:18 +0900

    Refactor : 상품썸네일

--- src/app/category/productlist/product/_components/Thumbnail.tsx
+++ src/app/category/productlist/product/_components/Thumbnail.tsx
@@ -4,6 +4,11 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 import { Pagination } from 'swiper/modules'
 
 function Thumnail() {
+    const productSThum = [
+        { id: 1, src: 'https://sitem.ssgcdn.com/19/39/55/item/1000581553919_i1_750.jpg', alt: '상품이미지1' },
+        { id: 2, src: 'https://sitem.ssgcdn.com/19/39/55/item/1000581553919_i2_750.jpg', alt: '상품이미지2' },
+        { id: 3, src: 'https://sitem.ssgcdn.com/19/39/55/item/1000581553919_i3_750.jpg', alt: '상품이미지3' },
+    ]
     return (
         <div>
             <Swiper
@@ -14,27 +19,11 @@ function Thumnail() {
                 }}
                 modules={[Pagination]}
             >
-                <SwiperSlide>
-                    <img
-                        className="w-full h-full"
-                        src="https://sitem.ssgcdn.com/19/39/55/item/1000581553919_i1_750.jpg"
-                        alt="상품이미지1"
-                    />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img
-                        className="w-full h-full"
-                        src="https://sitem.ssgcdn.com/19/39/55/item/1000581553919_i2_750.jpg"
-                        alt="상품이미지2"
-                    />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img
-                        className="w-full h-full"
-                        src="https://sitem.ssgcdn.com/19/39/55/item/1000581553919_i3_750.jpg"
-                        alt="상품이미지3"
-                    />
-                </SwiperSlide>
+                {productSThum.map((product) => (
+                    <SwiperSlide key={product.id}>
+                        <img className="w-full h-full" src={product.src} alt={product.alt} />
+                    </SwiperSlide>
+                ))}
             </Swiper>
             <div className=" flex justify-center items-center">
                 <div className="swiper-pagination   text-white text-xs bg-gray-300 rounded-full px-4 py-1  bg-opacity-70 "></div>
