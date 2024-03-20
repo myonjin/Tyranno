447a55a4d04979e9f332f3681f969279a87917f3
hyobin0726 <hyobin0726@naver.com>
2024-03-20 14:46:44 +0900

    Refactor : 상품상세정보

--- src/app/category/productlist/product/page.tsx
+++ src/app/category/productlist/product/page.tsx
@@ -1,37 +1,38 @@
 import Image from 'next/image'
 import React from 'react'
 import ProductBottomHeader from './_components/ProductBottomHeader'
-import ProductInformation from './_components/ProductInformation'
 import ProductDetail from './_components/ProductDetail'
 import BackIcon from '@/images/back'
 import CartIcon from '@/images/CartIcon.png'
 import SearchIcon from '@/images/SearchIcon.png'
-
-import 'swiper/css'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { Pagination } from 'swiper/modules'
 import Thumnail from './_components/Thumbnail'
 
 function Product() {
     return (
         <div>
-            <div className="fixed top-0 w-full flex items-center h-12 space-x-5 font-bold bg-white">
-                <span className="ml-2">
+            <div className="fixed top-0 w-full flex items-center justify-between h-12 space-x-5 font-bold bg-white px-2">
+                <span className="ml-4">
                     <BackIcon />
                 </span>
 
-                <p>상세</p>
-                <p>리뷰</p>
-                <p>Q&A</p>
+                <div className="flex justify-center flex-grow space-x-5">
+                    <p>상세</p>
+                    <p>리뷰</p>
+                    <p>Q&A</p>
+                </div>
 
-                <Image src={CartIcon} alt="장바구니아이콘"></Image>
+                <span className="mr-2">
+                    <Image src={CartIcon} alt="장바구니아이콘" width={24} height={24} />
+                </span>
 
-                <Image src={SearchIcon} alt="검색아이콘"></Image>
+                <span>
+                    <Image src={SearchIcon} alt="검색아이콘" width={24} height={24} />
+                </span>
             </div>
             <Thumnail />
 
-            <ProductInformation />
             <ProductDetail />
+
             <ProductBottomHeader />
         </div>
     )
