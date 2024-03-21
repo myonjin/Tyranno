3d3997a78953f9f5877efb7d8826fa7fff566bdc
jmlee119 <jmlee119@naver.com>
2024-03-21 11:27:07 +0900

    feat : main 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,9 +17,8 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={inter.className}>
-                {children} <Footer />
-            </body>
+            <body className={inter.className}>{children}</body>
+            <Footer />
         </html>
     )
 }
