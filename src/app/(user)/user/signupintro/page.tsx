bb63c34913e2f6eff1885175280da1e35f7fe9b5
jmlee119 <jmlee119@naver.com>
2024-03-22 19:02:24 +0900

    refactor : 필요없는 코드 삭제

--- src/app/(user)/user/signupintro/page.tsx
+++ src/app/(user)/user/signupintro/page.tsx
@@ -6,24 +6,22 @@ import Buttons from '@/components/ui/buttons'
 
 function Signup() {
     return (
-        <div>
+        <main>
             <HeaderTitle title="회원가입" />
             <h3 style={{ padding: '2%' }}>
                 믿고 사는 즐거움 <br />
                 SSG.COM에 오신것을 환영합니다.
             </h3>
-            <div>
-                <Image src={signupcoupon} alt="할인쿠폰안내" width={400} height={300} />
-            </div>
-            <div>
-                <span>
-                    <Buttons title="멤버십 혜택 받고 통홥회원 가입하기" href="/user/signupintro/auth" />
-                </span>
-                <label>
-                    <Buttons title="통합회원만 가입하기" color="#f0f0f0" href="/user/signupintro/auth" />
-                </label>
-            </div>
-        </div>
+
+            <Image src={signupcoupon} alt="할인쿠폰안내" width={400} height={300} />
+
+            <span>
+                <Buttons title="멤버십 혜택 받고 통홥회원 가입하기" href="/user/signupintro/auth" />
+            </span>
+            <label>
+                <Buttons title="통합회원만 가입하기" color="#f0f0f0" href="/user/signupintro/auth" />
+            </label>
+        </main>
     )
 }
 
