86da54c13f6e24c07949ce32d9a7cc06586d2167
hyun7494 <et009153@gmail.com>
2024-03-24 15:58:12 +0900

    Feat : Option Basic Code

--- src/main/java/com/tyranno/ssg/option/presentation/OptionController.java
+++ src/main/java/com/tyranno/ssg/option/presentation/OptionController.java
@@ -2,10 +2,25 @@ package com.tyranno.ssg.option.presentation;
 
 
 import com.tyranno.ssg.option.application.OptionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -15,11 +30,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/option")
 public class OptionController {
 
-         private final OptionService optionService;
+    private final OptionService optionService;
 
 
+    @Operation(summary = "선택 가능 옵션 조회", description = "선택 가능한 옵션을 조회한다.")
+    @GetMapping("/{productId}")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "선택 가능 옵션 조회 완료"),
+            @ApiResponse(responseCode = "400", description = "선택 가능 옵션 조회 중 오류 발생")})
+    public ResponseEntity<?> findOptions(@Parameter(description = "상품 ID") @PathVariable Long productId) {
 
-
+        return ResponseEntity.ok().body("오케이");
+    }
+}
 //         @GetMapping("/find-options")
 //         public List<Option> filter(
 //                 @RequestParam(value = "category", required = false) String category,
@@ -31,4 +54,75 @@ public class OptionController {
 //             log.info("category: {}, minPrice: {}, maxPrice: {}, price: {}, stock: {}", category, minPrice, maxPrice, price, stock);
 //             return optionService.filter(category, minPrice, maxPrice, price, stock);
 //         }
-}
+//@Slf4j
+//@RequiredArgsConstructor
+//@Tag(name = "공고", description = "공고 API")
+//@RestController
+//@RequestMapping("/posting")
+//public class PostingController {
+//
+//    private final PostingService postingService;
+//    @Tag(name = "공고")
+//    @Operation(summary = "공고 전체 수", description = "공고 전체 수를 조회한다.")
+//    @ApiResponses({
+//            @ApiResponse(responseCode = "200", description = "공고 전체 수를 조회 완료"),
+//            @ApiResponse(responseCode = "400", description = "공고 전체 수를 조회 중 오류 발생"),
+//            @ApiResponse(responseCode = "401", description = "로그인 필요")})
+//    @GetMapping("/count")
+//    private ResponseEntity<BasicResponse> countPosting() {
+//        log.info("countPosting - Call");
+//
+//        try {
+//            Integer postingCount = postingService.countPosting();
+//            return ResponseEntity.ok().body(BasicResponse.Body(ResponseCode.SUCCESS, new HashMap<String, Integer>() {{ put("count", postingCount); }}));
+//        } catch (RuntimeException e) {
+//            return ResponseEntity.badRequest().body(BasicResponse.Body(ResponseCode.REGIST_FAIL, null));
+//        }
+//    }
+//
+//
+//    @Tag(name = "공고")
+//    @Operation(summary = "공고 등록", description = "공고를 등록한다.")
+//    @ApiResponses({
+//            @ApiResponse(responseCode = "200", description = "공고 등록 완료"),
+//            @ApiResponse(responseCode = "400", description = "공고 등록 중 오류 발생"),
+//            @ApiResponse(responseCode = "401", description = "로그인 필요")})
+//    @PostMapping("")
+//    private ResponseEntity<BasicResponse> registPosting(@Parameter(description = "공고 등록을 위한 정보") @RequestBody BasicPostingRequest postingRegistRequest) {
+//        log.info("registPosting - Call");
+//
+//        try {
+//            postingService.registPosting(postingRegistRequest);
+//            return ResponseEntity.ok().body(BasicResponse.Body(ResponseCode.SUCCESS, null));
+//        } catch (RuntimeException e) {
+//            return ResponseEntity.badRequest().body(BasicResponse.Body(ResponseCode.REGIST_FAIL, null));
+//        }
+//    }
+//
+//    @Tag(name = "공고")
+//    @Operation(summary = "공고 목록 조회", description = "공고 전체 목록을 조회한다.")
+//    @ApiResponses({
+//            @ApiResponse(responseCode = "200", description = "공고 목록 조회 완료"),
+//            @ApiResponse(responseCode = "400", description = "공고 목록 조회 중 오류 발생")})
+//    @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+//    private ResponseEntity<BasicResponse> findAllPosting(@Parameter(description = "페이지", required = true) Integer page,
+//                                                         @Parameter(description = "사이즈", required = true) Integer size,
+//                                                         @Parameter(description = "프로젝트 주제") String subject,
+//                                                         @Parameter(description = "지역 코드") String localCode,
+//                                                         @Parameter(description = "분야 코드") String fieldCode,
+//                                                         @Parameter(description = "기술 스택 목록", schema = @Schema(type = "List")) @RequestParam(required = false) List<String> postingSkillList) {
+//        log.info("findAllPosting - Call");
+//
+//        Map<String, Object> searchKeys = new HashMap<>();
+//        if (subject != null && !subject.equals("")) searchKeys.put("subject", subject);
+//        if (localCode != null && !localCode.equals("")) searchKeys.put("localCode", localCode);
+//        if (fieldCode != null && !fieldCode.equals("")) searchKeys.put("fieldCode", fieldCode);
+//
+//        try {
+//            Map<String, Object> findAllPostingResponseList = postingService.findAllPosting(page, size, searchKeys, postingSkillList);
+//            return ResponseEntity.ok().body(BasicResponse.Body(ResponseCode.SUCCESS, findAllPostingResponseList));
+//        } catch (RuntimeException e) {
+//            e.printStackTrace();
+//            return ResponseEntity.badRequest().body(BasicResponse.Body(ResponseCode.LIST_NOT_FOUND, null));
+//        }
+//    }
