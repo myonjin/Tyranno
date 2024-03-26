9a8f652b0615d0891b612c607ff72c7c58b34a09
jmlee119 <jmlee119@naver.com>
2024-03-26 14:13:39 +0900

    feat : 오른쪽 스크롤 추가

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
-import MainEventSection from './MainEventSection'
+import MainEventSection from '../../components/pages/Main/MainEventSection'
 import MainHeader from '@/components/ui/MainHeader'
+import MainShortcutSection from '@/components/pages/Main/MainShortcutSection'
 
 export default function Home() {
     return (
         <main className="">
             {/* <div > */}
             <MainEventSection />
+            <MainShortcutSection />
             {/* </div> */}
         </main>
     )
