c4601b61745f5781d695215e0677f87de188a6c0
jmlee119 <jmlee119@naver.com>
2024-03-27 16:08:14 +0900

    feat: 하단 네비게이션 바

--- src/components/pages/Main/MainFooterNav.tsx
+++ src/components/pages/Main/MainFooterNav.tsx
@@ -1,15 +1,19 @@
 import { MainFooterNavData } from '@/lib/MainFooterNavData'
+import Image from 'next/image'
+
 export default function MainFooterNav() {
     return (
-        <nav className="z-[1] fixed right-0 left-0 bottom-0 bg-white w-full px-3">
-            <ul className="flex justify-between  " style={{ whiteSpace: 'nowrap' }}>
+        <nav className="z-[1] fixed right-0 left-0 bottom-0 bg-white w-full h-auto">
+            <ul className="flex justify-between " style={{ whiteSpace: 'nowrap' }}>
                 {MainFooterNavData.map((navs) => {
                     return (
-                        <li key={navs.id}>
-                            <a className="relative items-center" href={navs.url}>
-                                {/* <Image width={26} height={26} src="https://img.icons8.com/metro/26/menu.png" alt="menu" /> */}
-                                <span className="">{navs.title}</span>
-                            </a>
+                        <li key={navs.id} className="flex w-80 h-[50px] justify-center">
+                            <button className="">
+                                <a className="absolute  w-5 h-5 " href={navs.url}>
+                                    <Image src={navs.icon_url} alt="menu" fill />
+                                    <span className="text-xs">{navs.title}</span>
+                                </a>
+                            </button>
                         </li>
                     )
                 })}
