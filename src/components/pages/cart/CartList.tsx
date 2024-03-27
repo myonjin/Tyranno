9167d4f75f0a1edb2e7133c008f450cadf9a6bad
jmlee119 <jmlee119@naver.com>
2024-03-27 15:13:12 +0900

    refactor : 코드 리팩토링

--- src/components/pages/cart/CartList.tsx
+++ src/components/pages/cart/CartList.tsx
@@ -6,33 +6,11 @@ import './../../..//app/cart/cart.css'
 import Buttons from '@/components/ui/buttons'
 import { useRecoilState } from 'recoil'
 import { CartCheckedListAtom } from '@/state/CartCheckedListAtom'
+import { productData } from '@/lib/CartList'
 
 export default function CartList() {
     const [recoilSample, setRecoilSample] = useRecoilState<number[]>(CartCheckedListAtom)
-    const productData = [
-        {
-            productId: 1,
-            vendorName: '노브랜드',
-            name: '상품1',
-            productPrice: 10000,
-            discount: 0,
-            count: 1,
-            isIncluded: 11,
-            isKeep: 99,
-            imageUrl: 'https://sitem.ssgcdn.com/89/11/90/item/1000043901189_i1_750.jpg',
-        },
-        {
-            productId: 12,
-            vendorName: '노브랜드',
-            name: '상품2',
-            productPrice: 20000,
-            discount: 10,
-            count: 2,
-            isIncluded: 11,
-            isKeep: 99,
-            imageUrl: 'https://sitem.ssgcdn.com/89/11/90/item/1000043901189_i1_750.jpg',
-        },
-    ]
+
     const [filteredProductList, setFilteredProductList] = useState(
         productData.filter((product) => product.isIncluded === 11),
     )
