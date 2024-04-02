93243fab1b6f87198c3fa7af9b08a80a500e4621
sunju <iseonju745@gmail.com>
2024-04-02 09:41:06 +0900

    Fix: cors 영민님꺼

--- src/main/java/com/tyranno/ssg/security/CorsConfig.java
+++ src/main/java/com/tyranno/ssg/security/CorsConfig.java
@@ -1,10 +1,13 @@
-//package com.tyranno.ssg.security;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
+package com.tyranno.ssg.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
 //@Configuration
 //public class CorsConfig implements WebMvcConfigurer {
 //    // CORS 설정
@@ -13,8 +16,27 @@
 //        registry.addMapping("/**")
 //                .allowedMethods(CorsConfiguration.ALL)
 //                .allowedHeaders(CorsConfiguration.ALL)
-//                .allowedOriginPatterns(CorsConfiguration.ALL) //모든 ip 에 응답 허용
+////                .allowedOriginPatterns(CorsConfiguration.ALL) //모든 ip 에 응답 허용
 //                .exposedHeaders(CorsConfiguration.ALL)
 //                .allowCredentials(true);
 //    }
 //}
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*"); // 허용할 URL
+        config.addAllowedHeader("*"); // 허용할 Header
+        config.addAllowedMethod("*"); // 허용할 Http Method
+        // CORS 는 해결했지만 프론트에 응답 헤더에 추가한 Authorization 이 전달되지 않는 문제 해결
+        config.setExposedHeaders(Arrays.asList("Authorization", "refreshToken"));
+        source.registerCorsConfiguration("/**", config);
+        // 모든 Url 대해 설정한 CorsConfiguration 등록
+        return new CorsFilter(source);
+    }
+}
