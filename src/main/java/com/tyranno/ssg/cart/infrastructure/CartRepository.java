a0992e1c602cc916ac70968415d8bc6a42825a6e
sunju <iseonju745@gmail.com>
2024-04-02 18:15:57 +0900

    Feat: delivery baseName 조회, post 추가

--- src/main/java/com/tyranno/ssg/cart/infrastructure/CartRepository.java
+++ src/main/java/com/tyranno/ssg/cart/infrastructure/CartRepository.java
@@ -1,6 +1,7 @@
 package com.tyranno.ssg.cart.infrastructure;
 
 import com.tyranno.ssg.cart.domain.Cart;
+import com.tyranno.ssg.delivery.domain.Delivery;
 import com.tyranno.ssg.option.domain.Option;
 import com.tyranno.ssg.users.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,4 +14,5 @@ public interface CartRepository extends JpaRepository<Cart, Long> {
     boolean existsByOptionAndUsers(Option option, Users users);
     Cart findByOptionAndUsers(Option option, Users users); // 먼저 existsByOption 로 검사해서 optional 형식으로 리턴하지 않음
     void deleteByIdIn(List<Long> ids);
+
 }
