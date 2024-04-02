08a738f48e91bc2a9268725aabb3691963a55563
sunju <iseonju745@gmail.com>
2024-04-02 18:22:34 +0900

    Feat: 인증번호 tag 달기

--- src/main/java/com/tyranno/ssg/text/SmsController.java
+++ src/main/java/com/tyranno/ssg/text/SmsController.java
@@ -1,6 +1,8 @@
 package com.tyranno.ssg.text;
 
 import com.tyranno.ssg.global.ResponseEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import net.nurigo.sdk.message.response.SingleMessageSentResponse;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,18 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "문자인증", description = "Text API")
 @RequestMapping("/api/v1/auth")
 public class SmsController {
 
     private final SmsService smsService;
 
     // 인증번호 전송
+    @Operation(summary = "인증번호 전송", description = "인증번호 4자리를 전송한다.")
     @PostMapping("/send")
     public SingleMessageSentResponse sendOne(@RequestBody SmsSendDto smsSendDto) {
         return smsService.sendOne(smsSendDto);
     }
 
     // 인증번호 확인
+    @Operation(summary = "인증번호 확인", description = "문자 인증번호가 맞는지 확인한다.")
     @PostMapping("/verify")
     public ResponseEntity<?> verifySms(@RequestBody SmsCertificationDto requestDto) {
         smsService.verifySms(requestDto);
