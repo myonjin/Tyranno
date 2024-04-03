6ee5ea2864277d7eb2b70be0dff219b6238f0c2f
sunju <iseonju745@gmail.com>
2024-04-03 20:18:02 +0900

    Fix: 배송지 localDB는 됨

--- src/main/java/com/tyranno/ssg/delivery/presentation/DeliveryController.java
+++ src/main/java/com/tyranno/ssg/delivery/presentation/DeliveryController.java
@@ -77,7 +77,7 @@ public class DeliveryController {
 
     @Operation(summary = "기본 배송지 별칭 조회", description = "한 유저의 기존 배송지 별칭을 조회한다.(기존배송지로 지정하려는 delivery의 id 입력)")
     @GetMapping("/base-name")
-    public ResponseEntity<?> GetBaseDeliveryName(@RequestHeader("Authorization") String token) {
+    public ResponseEntity<?> getBaseDeliveryName(@RequestHeader("Authorization") String token) {
         String uuid = jwtTokenProvider.tokenToUuid(token);
 
         return new ResponseEntity<>(deliveryService.getBaseDeliveryName(uuid));
