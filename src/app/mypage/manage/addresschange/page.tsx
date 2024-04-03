55f9cad0d0485c2970cf066fa2eec004546e1566
jmlee119 <jmlee119@naver.com>
2024-04-03 10:12:47 +0900

    배송지 변경 창

---
+++ src/app/mypage/manage/addresschange/page.tsx
@@ -0,0 +1,9 @@
+import ChangeAddress from '@/components/pages/mypage/ChangeAddress'
+
+export default function AddressChange() {
+    return (
+        <div>
+            <ChangeAddress />
+        </div>
+    )
+}
