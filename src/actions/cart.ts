2b8141b3eefe314ebb42f9ab9d1b51d34f83a8ee
jmlee119 <jmlee119@naver.com>
2024-04-07 02:31:19 +0900

    feat : 장바구니 옵션

--- src/actions/cart.ts
+++ src/actions/cart.ts
@@ -1,3 +1,4 @@
+import { options } from '@/app/api/auth/[...nextauth]/options'
 import { cartCountDataType, clickDeleteDataType, isKeepDataType } from '@/types/CartDataType'
 import { DeleteAPI, GetAPI, PostAPI, PutAPI } from './FetchAPI'
 import { GetToken } from './GetToken'
@@ -22,4 +23,8 @@ async function isKeepAPI(isKeep: isKeepDataType) {
     const response = await PutAPI('/api/v1/cart/keep', isKeep, await token)
     return response.result
 }
-export { getCartListAPI, deleteCartIdAPI, countCartAPI, isKeepAPI, deleteClickAPI }
+async function getOptionsAPI(option_id: number) {
+    const response = await GetAPI(`/api/v1/option/names/${option_id}`, { option_id }, await token)
+    return response.result
+}
+export { getCartListAPI, deleteCartIdAPI, countCartAPI, isKeepAPI, deleteClickAPI, getOptionsAPI }
