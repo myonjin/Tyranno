c7e3e6ddf0581a1862d7417d038bd7b0ae5779a2
jmlee119 <jmlee119@naver.com>
2024-04-07 13:33:47 +0900

    feat : 장바구니 메인 주소 출력

--- src/components/pages/cart/DeliveryAddress.tsx
+++ src/components/pages/cart/DeliveryAddress.tsx
@@ -1,37 +1,43 @@
+'use client'
+import { getMainDeliveryAPI } from '@/actions/delivery'
+import { CartAddressDataType } from '@/types/AddressDataType'
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
 
 export default function DeliveryAddress() {
-    const addData = [
-        {
-            id: 1,
-            name: '자취방',
-            zipCode: 12312,
-            addressBase: '부산광역시 센텀',
-            addressDetail: '스파로스',
-            isMain: '기본배송지',
-        },
-    ]
+    const [mainAddress, setMainAddress] = useState<CartAddressDataType>()
+    const fetchData = async () => {
+        try {
+            const res = await getMainDeliveryAPI()
+            setMainAddress(res as CartAddressDataType)
+        } catch (err) {
+            console.error(err)
+        }
+    }
+    useEffect(() => {
+        fetchData()
+    }, [])
+
     return (
         <section className="my-5 mx-5 z-[2]">
             <div className="flex mb-2 ">
-                {/* <Image src={LocationIcon} alt="위치" width={20} /> */}
                 <Image
                     width={20}
                     height={20}
                     src="https://img.icons8.com/ios/50/place-marker--v1.png"
                     alt="place-marker--v1"
                 />
-                <h3 className="text- base font-bold">{addData[0].name}</h3>
+                <h3 className="text- base font-bold">{mainAddress?.deliveryName}</h3>
                 <span
                     className="ml-1 px-2 text-sm justify-center items-center flex"
                     style={{ backgroundColor: '#ff5452', color: 'white' }}
                 >
-                    {addData[0].isMain}
+                    기본배송지
                 </span>
             </div>
 
             <p className="text-sm">
-                [{addData[0].zipCode}] {addData[0].addressBase} {addData[0].addressDetail}{' '}
+                [{mainAddress?.zipCode}] {mainAddress?.deliveryBase} {mainAddress?.deliveryDetail}{' '}
             </p>
         </section>
     )
