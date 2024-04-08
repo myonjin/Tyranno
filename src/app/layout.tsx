9a73e2af8d29a6716a7b32d07e37df1571901893
hyobin0726 <hyobin0726@naver.com>
2024-04-08 19:31:54 +0900

    style : 파비콘 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
     title: '믿고 사는 즐거움 SSG.COM',
     description: '믿고 사는 즐거움 SSG.COM',
-    icons: {},
+    icons: {
+        icon: '/favicon.ico',
+    },
 }
 
 export default function RootLayout({
