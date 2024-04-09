5024c5727117598a6aae260867cfee0dbc2a1abe
sunju <iseonju745@gmail.com>
2024-04-09 18:52:12 +0900

    Feat: kakaopay 구현완료

--- src/main/java/com/tyranno/ssg/payment/presentation/PaymentController.java
+++ src/main/java/com/tyranno/ssg/payment/presentation/PaymentController.java
@@ -3,9 +3,7 @@ package com.tyranno.ssg.payment.presentation;
 import com.tyranno.ssg.global.GlobalException;
 import com.tyranno.ssg.global.ResponseEntity;
 import com.tyranno.ssg.global.ResponseStatus;
-import com.tyranno.ssg.payment.application.PaymentService;
-import com.tyranno.ssg.payment.dto.ApproveRequestDto;
-import com.tyranno.ssg.payment.dto.ApproveResponseDto;
+import com.tyranno.ssg.payment.application.PaymentServiceImp;
 import com.tyranno.ssg.payment.dto.ReadyRequestDto;
 import com.tyranno.ssg.payment.dto.ReadyResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
@@ -22,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class PaymentController {
 
-    private final PaymentService paymentService;
+    private final PaymentServiceImp paymentService;
 
     @Operation(summary = "카카오페이 결제 시작", description = "카카오페이 결제를 시작한다.")
     @PostMapping("/ready")
@@ -36,7 +34,7 @@ public class PaymentController {
      */
     @Operation(summary = "카카오페이 결제 성공", description = "카카오페이 결제를 성공하여 pg_token을 리턴한다.")
     @GetMapping("/success/{id}")
-    public ResponseEntity<?> approveKakaoPay(@Parameter String id, @RequestParam("pg_token") String pgToken) {
+    public ResponseEntity<?> approveKakaoPay(@PathVariable String id, @RequestParam("pg_token") String pgToken) {
         return new ResponseEntity<>(paymentService.getKakaoPayApprove(id, pgToken));
     }
 //    @Operation(summary = "카카오페이 승인 요청", description = "카카오페이 결제 승인을 요청한다.")
