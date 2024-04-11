941ace647a00150f4eafc9e084151ad98f79565f
jmlee119 <jmlee119@naver.com>
2024-04-11 10:13:45 +0900

    feat : result token 수정

--- src/actions/GetToken.tsx
+++ src/actions/GetToken.tsx
@@ -2,20 +2,19 @@
 import { getSession } from 'next-auth/react'
 
 interface tokenType {
-    code: number
-    exp: number
-    iat: number
     isSuccess: boolean
-    jti: string
     message: string
+    code: number
     result: string
+    iat: number
+    exp: number
+    jti: string
 }
 
 const GetToken = async () => {
     const session = await getSession()
-    console.log('session', session)
-    const token = session?.user as tokenType
-    return token.result
+    const token = session?.user?.result || ''
+    return token
 }
 
 export { GetToken }
